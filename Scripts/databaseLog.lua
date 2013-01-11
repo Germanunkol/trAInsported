5233545b2650069f1a724b919b44d3369394f58d
Germanunkol <Germanunkol@yahoo.com>
2013-01-11 21:06:11 +0100

    Added next match logging.

--- Scripts/databaseLog.lua
+++ Scripts/databaseLog.lua
@@ -216,7 +216,9 @@ function chooseAIfromDB(numMatches)
 				for count = 1, numMatches + 1 do
 					cursor,err = conn:execute("SELECT name,owner FROM nextMatch WHERE matchNum=" .. count .. ";")
 					
-					row = assert(cursor:fetch ({}, "a"))
+					if cursor and type("cursor") ~= "number" then
+						row = cursor:fetch ({}, "a"))
+					end
 					print(cursor, err, row)
 					if not cursor or cursor == 0 or not row then
 					

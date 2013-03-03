f522382c7adc6d5751cbb67f8fb3bbe359fcdf3f
Germanunkol <Germanunkol@yahoo.com>
2013-03-04 00:50:23 +0100

    Removed logging of Matches on server

--- Scripts/sendPackets.lua
+++ Scripts/sendPackets.lua
@@ -18,6 +18,8 @@ function sendPackets.add(packetID, text, time)
 	sendPacketsList[packetID] = {ID = packetID, time=time, event = text}
 	-- table.sort(sendPacketsList, sortByTime)
 	
+	-- log:
+	--[[
 	if text:find("END_ROUND") then
 		s = ""
 		for k = 1, #sendPacketsList do
@@ -25,6 +27,7 @@ function sendPackets.add(packetID, text, time)
 		end
 		love.filesystem.write("match.txt", s)
 	end
+	]]--
 end
 
 function sendPackets.getPacketNum()

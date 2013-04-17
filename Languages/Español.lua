285fba56db2b5d531cd408aa3d9a763d1dc67b07
Germanunkol <Germanunkol@yahoo.com>
2013-04-17 11:08:07 +0200

    Added Spanish translation

---
+++ Languages/Español.lua
@@ -0,0 +1,109 @@
+
+-- Language file for trAInsported. Make a copy of this to translate into your own language.
+-- Please don't change the order of the entries in this file.
+
+
+-------------------------------------------------
+-- Menu:
+-------------------------------------------------
+LNG.menu_live = [[En linea]]
+LNG.menu_live_tooltip = [[¡Mira partidas en linea y en directo!]]
+LNG.menu_tutorial = [[Tutoriales]]
+LNG.menu_tutorial_tooltip = [[¡Aprende a jugar!]]
+LNG.menu_challenge = [[Desafío]]
+LNG.menu_challenge_tooltip = [[¡Vence los mapas de desafío! Obtiene más mapas en el sitio web del juego.]]
+LNG.menu_compete = [[Competencia]]
+LNG.menu_compete_tooltip = [[Crea una partida de prueba para tu AI]]
+LNG.menu_random = [[Aleatorio]]
+LNG.menu_random_tooltip = [[Empieza una partida aleatoria en un mapa aleatorio usando AIS aleatorias de el directorio 'AI']]
+LNG.menu_settings = [[Configuración]]
+LNG.menu_settings_tooltip = [[Cambiar idioma, resolución, etc.]]
+LNG.menu_exit = [[Salir]]
+
+LNG.menu_return = [[Regresar]]
+LNG.menu_return_to_main_menu_tooltip = [[Volver al menú principal]]
+
+LNG.menu_choose_ai = [[Elige las AIS para la partida:]]
+LNG.menu_choose_ai_tooltip = [[Elige las AIS que competirán en la partida]]
+
+LNG.menu_choose_dimensions = [[Largo y alto:]]
+LNG.menu_choose_dimensions_tooltip1 = [[Selecciona el largo]]
+LNG.menu_choose_dimensions_tooltip2 = [[Selecciona el alto]]
+
+LNG.menu_choose_timemode = [[Tiempo y modo:]]
+
+LNG.menu_choose_region = [[Región:]]
+
+LNG.menu_start = [[Comenzar]]
+LNG.menu_start_tooltip = [[Comienza la partida con la configuración seleccionada]]
+
+LNG.menu_main_server = [[Server principal]]
+LNG.menu_main_server_tooltip = [[Conectar al server principal. ¡Debes estar conectado a Internet!]]
+LNG.menu_local_server = [[Servidor local]]
+LNG.menu_local_server_tooltip = [[Conectar a un server que se encuentra en esta computadora.]]
+
+-- Settings menu:
+LNG.menu_settings_resolution = [[Tamaño de pantalla:]]
+LNG.menu_resolution_tooltip = [[Selecciona la nueva resolución]]
+LNG.menu_settings_options = [[Opciones:]]
+LNG.menu_clouds_on = [[Nubes: Si]]
+LNG.menu_clouds_off = [[Nubes: No]]
+LNG.menu_clouds_on_tooltip = [[Click para activar las nubes.]]
+LNG.menu_clouds_off_tooltip = [[Click para desactivar las nubes.]]
+LNG.menu_settings_language = [[Lenguaje:]]
+
+-- Menu errors:
+LNG.menu_err_min_ai = [[¡Necesitas seleccionar al menos una AI!]]
+LNG.menu_err_dimensions = [[¡Dimensiones del mapa incorrectas!]]
+LNG.menu_err_mode = [[¡Modo de juego incorrecto!]]
+LNG.menu_err_time = [[¡Horario de juego incorrecto!]]
+LNG.menu_err_resolution = [[¡Error al establecer la nueva resolución!]]
+
+
+-------------------------------------------------
+-- INGAME:
+-------------------------------------------------
+-- Fast forward message:
+LNG.fast_forward = [[AVANCE RÁPIDO PARA SINCRONIZAR CON EL SERVIDOR]]
+LNG.confirm_leave = [[¿Quieres salir de la partida y regresar al menú principal?]]
+
+LNG.reload = [[Recargar]]
+LNG.reload_confirm = [[¿Seguro qué quieres recargar las AIs?]]
+LNG.reload_tooltip = [[Recarga los scripts de las AI y empieza de nuevo la partida.]]
+LNG.disconnect = [[Desconectar]]
+LNG.end_match = [[Salir]]
+
+LNG.speed_up = [[Aumenta la velocidad]]
+LNG.slow_down = [[Disminuye la velocidad]]
+LNG.pause = [[Pausa el juego]]
+
+LNG.by = [[por]]
+LNG.transported = [[Transportados]]
+-- the following three strings are for the string "X of Y passengers"
+LNG.transported1 = [[]]		-- before X
+LNG.transported2 = [[ de ]]		-- between X and Y
+LNG.transported3 = [[ pasajeros]]		-- after Y
+
+LNG.round_ends = [[La partida termina en]]		-- after Y
+LNG.seconds = [[seg]]		-- after Y
+LNG.minutes = [[min]]		-- after Y
+LNG.hours = [[h]]		-- after Y
+LNG.days = [[dias]]		-- after Y
+LNG.end_of_match = [[¡La partida ha acabado!]]		-- after Y
+
+-------------------------------------------------
+-- MESSAGE BOX:
+-------------------------------------------------
+LNG.exit_confirm = [[¿Seguro qué quieres salir?]]
+LNG.confirm_resolution = [[¿Mantienes la nueva resolución?]]
+LNG.agree = [[Si]]
+LNG.disagree = [[No]]
+LNG.cancel = [[Cancelar]]
+
+-------------------------------------------------
+-- ERRORS:
+-------------------------------------------------
+LNG.err_already_generating_map = [[Ya se está generando un nuevo mapa, espera que termine...]]
+LNG.err_wait_for_rendering = [[Espera a que el renderizado termine...]]
+LNG.err_rendering = [[Algo salió mal en un thread mientras se generaba el mapa, por favor reinicia el juego e intenta denuevo.]]
+LNG.err_already_connecting = [[Ya se está intentando establecer conexión.]]

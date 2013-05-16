310b35eabf0de07ee4049fc8cd5060641c91e814
Eero Penttilä <pena8666@gmail.com>
2013-05-16 22:16:46 +0300

    Update Finnish.lua
    Translation part 2/2.

--- Languages/Finnish.lua
+++ Languages/Finnish.lua
@@ -1,4 +1,3 @@
-
 -- Language file for trAInsported. Make a copy of this to translate into your own language.
 -- Please don't change the order of the entries in this file.
 
@@ -13,8 +12,8 @@ LNG.load_generation = {}
 -------------------------------------------------
 -- Menu:
 -------------------------------------------------
-LNG.menu_live = [[Live]]
-LNG.menu_live_tooltip = [[Katso otteluita verkossa]]
+LNG.menu_live = [[Ottelut verkossa]]
+LNG.menu_live_tooltip = [[Katso suoria otteluita verkossa]]
 LNG.menu_tutorial = [[Harjoittele]]
 LNG.menu_tutorial_tooltip = [[Opi perusteet tekoälyn tekemisestä]]
 LNG.menu_challenge = [[Haasteet]]
@@ -62,134 +61,134 @@ LNG.menu_local_server = [[Paikallinen palvelin]]
 LNG.menu_local_server_tooltip = [[Yhdistä tällä koneella olevalle palvelimelle]]
 
 -- Settings menu:
-LNG.menu_settings_resolution = [[Screen size:]]
-LNG.menu_resolution_tooltip = [[Set new screen resolution]]
-LNG.menu_settings_options = [[Options:]]
-LNG.menu_clouds_on = [[Clouds: On]]
-LNG.menu_clouds_off = [[Clouds: Off]]
-LNG.menu_clouds_on_tooltip = [[Click to enable cloud rendering.]]
-LNG.menu_clouds_off_tooltip = [[Click to disable cloud rendering.]]
-LNG.menu_settings_language = [[Language:]]
-LNG.menu_settings_language_tooltip1 = [[Click to switch game language to]]  -- before lang name
+LNG.menu_settings_resolution = [[Ruudun resoluutio:]]
+LNG.menu_resolution_tooltip = [[Valitse uusi resoluutio]]
+LNG.menu_settings_options = [[Asetukset:]]
+LNG.menu_clouds_on = [[Pilvet: Kyllä]]
+LNG.menu_clouds_off = [[Pilvet: Ei]]
+LNG.menu_clouds_on_tooltip = [[Ota pilvet käyttöön]]
+LNG.menu_clouds_off_tooltip = [[Ota pilvet pois käytöstä]]
+LNG.menu_settings_language = [[Kieli:]]
+LNG.menu_settings_language_tooltip1 = [[Valitse kieleksi: ]]  -- before lang name
 LNG.menu_settings_language_tooltip2 = [[]]	-- after lang name
 
 -- Menu errors:
-LNG.menu_err_min_ai = [[Need to choose at least one AI!]]
-LNG.menu_err_dimensions = [[Invalid map dimensions!]]
-LNG.menu_err_mode = [[Invalid game mode!]]
-LNG.menu_err_time = [[Invalid game time!]]
-LNG.menu_err_resolution = [[Failed to set resolution!]]
+LNG.menu_err_min_ai = [[Sinun tulee valita vähintään yksi tekoäly!]]
+LNG.menu_err_dimensions = [[Kartan koko virheellinen!]]
+LNG.menu_err_mode = [[Pelityyppi virheellinen!]]
+LNG.menu_err_time = [[Peliaika virheellinen!]]
+LNG.menu_err_resolution = [[Resoluution käyttöönotto epäonnistui!]]
 
 
 -------------------------------------------------
 -- INGAME:
 -------------------------------------------------
 -- Fast forward message:
-LNG.fast_forward = [[FAST FORWARD TO CATCH UP WITH SERVER]]
-LNG.confirm_leave = [[Leave the current match and return to menu?]]
+LNG.fast_forward = [[Kelataan pelitapahtumia, että päästään samaan tilanteeseen palvelimen kanssa]]
+LNG.confirm_leave = [[Poistu nykyisestä ottelusta ja palaa päävalikkoon?]]
 
-LNG.open_folder = [[Open Folder]]
-LNG.open_folder_tooltip = [[Opens the folder: AI_FOLDER_DIRECTORY]] -- AI_FOLDER_DIRECTORY will be replaced by the game with the correct directory.
+LNG.open_folder = [[Avaa kansio]]
+LNG.open_folder_tooltip = [[Avaa kansion: AI_FOLDER_DIRECTORY]] -- AI_FOLDER_DIRECTORY will be replaced by the game with the correct directory.
 
-LNG.reload = [[Reload]]
-LNG.reload_confirm = [[Reload the AIs?]]
-LNG.reload_tooltip = [[Reloads the AI scripts and restarts the round.]]
-LNG.disconnect = [[Disconnect]]
-LNG.end_match = [[End Match]]
+LNG.reload = [[Päivitä]]
+LNG.reload_confirm = [[Päivitä tekoälyt?]]
+LNG.reload_tooltip = [[Lataa päivitetyt tekoälyt ja aloittaa ottelun alusta.]]
+LNG.disconnect = [[Katkaise yhteys]]
+LNG.end_match = [[Päätä ottelu]]
 
-LNG.speed_up = [[Speeds game up]]
-LNG.slow_down = [[Slows game down]]
-LNG.pause = [[Pauses game]]
+LNG.speed_up = [[Nopeuta peliä]]
+LNG.slow_down = [[Hidasta peliä]]
+LNG.pause = [[Pysäytä peli]]
 
-LNG.by = [[by]]
-LNG.transported = [[Transported]]
+LNG.by = [[]]
+LNG.transported = [[Kuljettanut]]
 -- the following three strings are for the string "X of Y passengers"
 LNG.transported1 = [[]]		-- before X
-LNG.transported2 = [[ of ]]		-- between X and Y
-LNG.transported3 = [[ passengers]]		-- after Y
+LNG.transported2 = [[ / ]]		-- between X and Y
+LNG.transported3 = [[ matkustajasta]]		-- after Y
 
-LNG.round_ends = [[Round ends in]]
-LNG.seconds = [[secs]]
+LNG.round_ends = [[Ottelua jäljellä]]
+LNG.seconds = [[s]]
 LNG.minutes = [[min]]
-LNG.hours = [[h]]
-LNG.days = [[days]]
-LNG.end_of_match = [[Match is over!]]
+LNG.hours = [[tuntia]]
+LNG.days = [[päivää]]
+LNG.end_of_match = [[Ottelu on päättynyt!]]
 
-LNG.live_match = [[LIVE MATCH]]
-LNG.lost_connection = [[LOST CONNECTION]]
+LNG.live_match = [[Suora ottelu]]
+LNG.lost_connection = [[Yhteys katkennut]]
 
 -------------------------------------------------
 -- LOADING SCREEN:
 -------------------------------------------------
-LNG.load_new_map = [[New Map]]
-LNG.load_map_size = [[Size: ]]
-LNG.load_map_time = [[Time: ]]
-LNG.load_map_mode_time = [[Mode: Round time: ]]
-LNG.load_map_mode_passengers = [[Mode: Transport enough Passengers]]
-LNG.load_generating_map = [[Generating Map]]
-LNG.load_rendering_map = [[Rendering Map]]
-LNG.load_connecting = [[Connecting]]
-LNG.load_failed = [[Failed!]]
+LNG.load_new_map = [[Uusi kartta]]
+LNG.load_map_size = [[Koko: ]]
+LNG.load_map_time = [[Aika: ]]
+LNG.load_map_mode_time = [[Pelityyppi: Ottelun ajankohta: ]]
+LNG.load_map_mode_passengers = [[Pelityyppi: Kuljeta eniten matkustajia]]
+LNG.load_generating_map = [[Luodaan karttaa]]
+LNG.load_rendering_map = [[Renderöidään karttaa]]
+LNG.load_connecting = [[Yhdistetään]]
+LNG.load_failed = [[Epäonnistui!]]
 -- leave ["rails"] etc, just translate the right part of the equal sign:
-LNG.load_generation["rails"] = [[Rails]]
-LNG.load_generation["houses"] = [[Houses]]
-LNG.load_generation["hotspots"] = [[Hotspots]]
+LNG.load_generation["rails"] = [[Radat]]
+LNG.load_generation["houses"] = [[Talot]]
+LNG.load_generation["hotspots"] = [[Ruuhkapisteet]]
 
 -------------------------------------------------
 -- ROUND END STATISTICS:
 -------------------------------------------------
 -- The round statistics here are placed together using these strings as base. Some have versions in plural and in singular forms. If there's no difference in your language, please just copy the one version into both strings - do NOT delete the singular form!
 -- In these strings, _AINAME_ and _NUMBER_ will be replaced by the appropriate values, so make sure to include them!
-LNG.stat_most_picked_up_title = [[Hospitality]]
-LNG.stat_most_picked_up = [[AI _AINAME_ picked up _NUMBER_ passengers.]]
-LNG.stat_most_picked_up_sing = [[AI _AINAME_ picked up _NUMBER_ passenger.]]
+LNG.stat_most_picked_up_title = [[Vieraanvaraisuus]]
+LNG.stat_most_picked_up = [[Tekoäly _AINAME_ poimi kyytiin _NUMBER_ matkustajaa.]]
+LNG.stat_most_picked_up_sing = [[Tekoäly _AINAME_ poimi kyytiin _NUMBER_ matkustajan.]]
 
-LNG.stat_most_trains_title = [[Fleetus Maximus]]
-LNG.stat_most_trains = [[AI _AINAME_ owned _NUMBER_ trains.]]
-LNG.stat_most_trains_sing = [[AI _AINAME_ owned _NUMBER_ trains]]
+LNG.stat_most_trains_title = [[Junanrakentaja]]
+LNG.stat_most_trains = [[Tekoäly _AINAME_ hankki _NUMBER_ junaa.]]
+LNG.stat_most_trains_sing = [[Tekoäly _AINAME_ hankki _NUMBER_ junan.]]
 
-LNG.stat_most_transported_title = [[Earned Your Pay]]
-LNG.stat_most_transported = [[AI _AINAME_ brought _NUMBER_ passengers to their destinations.]]
-LNG.stat_most_transported_sing = [[AI _AINAME_ brought _NUMBER_ passenger to her/his destination.]]
+LNG.stat_most_transported_title = [[Ansainnut palkkansa]]
+LNG.stat_most_transported = [[Tekoäly _AINAME_ kuljetti _NUMBER_ matkustajaa heidän päämääräänsä.]]
+LNG.stat_most_transported_sing = [[Tekoäly _AINAME_ kuljetti _NUMBER_ matkustajan hänen päämääräänsä.]]
 
-LNG.stat_most_normal_transported_title = [[Socialist]]
-LNG.stat_most_normal_transported = [[AI _AINAME_ brought _NUMBER_ non-VIP passengers to their destinations.]]
-LNG.stat_most_normal_transported_sing = [[AI _AINAME_ brought _NUMBER_ non-VIP passenger to her/his destination.]]
+LNG.stat_most_normal_transported_title = [[Sosialisti]]
+LNG.stat_most_normal_transported = [[Tekoäly _AINAME_ kuljetti _NUMBER_  ei VIP matkustajaa heidän päämääräänsä.]]
+LNG.stat_most_normal_transported_sing = [[Tekoäly _AINAME_ kuljetti _NUMBER_ ei VIP matkustajan hänen päämääräänsä.]]
 
-LNG.stat_dropped_title = [[Get lost...]]
-LNG.stat_dropped = [[AI _AINAME_ dropped off _NUMBER_ passengers where they didn't want to go.]]
-LNG.stat_dropped_sing = [[AI _AINAME_ brought _NUMBER_ passenger where he/she didn't want to go.]]
+LNG.stat_dropped_title = [[Häivy!]]
+LNG.stat_dropped = [[Tekoäly _AINAME_ jätti _NUMBER_ matkustajaa radanvarteen.]]
+LNG.stat_dropped_sing = [[Tekoäly _AINAME_ jätti _NUMBER_ matkustajan radanvarteen.]]
 
-LNG.stat_most_money_title = [[Capitalist]]
-LNG.stat_most_money = [[AI _AINAME_ earned _NUMBER_ credits.]]
+LNG.stat_most_money_title = [[Kapitalisti]]
+LNG.stat_most_money = [[Tekoäly _AINAME_ ansaitsi _NUMBER_ krediittiä.]]
 
 -- Some of the following can take up to three arguments: _NUMBER_, _AINAME_ and _TRAINNAME_:
-LNG.stat_tr_most_picked_up_title = [[Busy little Bee!]]
-LNG.stat_tr_most_picked_up = [[_TRAINNAME_ [_AINAME_] picked up more passengers than any other train.]]
+LNG.stat_tr_most_picked_up_title = [[Ahkera pikku mehiläinen]]
+LNG.stat_tr_most_picked_up = [[Juna _TRAINNAME_ [_AINAME_] poimi kyytiin eniten matkustajia.]]
 
-LNG.stat_tr_most_transported_title = [[Home sweet Home]]
-LNG.stat_tr_most_transported = [[_TRAINNAME_ [_AINAME_] brought more passengers to their destinations than any other train.]]
+LNG.stat_tr_most_transported_title = [[Oma koti kullan kallis]]
+LNG.stat_tr_most_transported = [[Juna _TRAINNAME_ [_AINAME_] kuljetti perille eniten matkustajia.]]
 
-LNG.stat_tr_dropped_title = [[Why don't you walk?]]
-LNG.stat_tr_dropped = [[_TRAINNAME_ [_AINAME_] left _NUMBER_ passengers in the middle of nowhere!]]
-LNG.stat_tr_dropped_sing = [[_TRAINNAME_ [_AINAME_] left _NUMBER_ passenger in the middle of nowhere!]]
+LNG.stat_tr_dropped_title = [[Entä jos kävelisit?]]
+LNG.stat_tr_dropped = [[Juna _TRAINNAME_ [_AINAME_] jätti _NUMBER_ matkustajaa radanvarteen.]]
+LNG.stat_tr_dropped_sing = [[Juna _TRAINNAME_ [_AINAME_] jätti _NUMBER_ matkustajan radanvarteen.]]
 
-LNG.stat_tr_blocked_title = [[Line is busy...]]
-LNG.stat_tr_blocked = [[_TRAINNAME_ [_AINAME_] was blocked for a total of _NUMBER_ seconds.]]
+LNG.stat_tr_blocked_title = [[Raide varattu...]]
+LNG.stat_tr_blocked = [[Juna _TRAINNAME_ [_AINAME_] oli juuttuneena ruuhkaan yhteensä _NUMBER_ sekuntia.]]
 
 -------------------------------------------------
 -- MESSAGE BOX:
 -------------------------------------------------
-LNG.exit_confirm = [[Sure you want to quit?]]
-LNG.confirm_resolution = [[Keep this new resolution?]]
-LNG.agree = [[Yes]]
-LNG.disagree = [[No]]
-LNG.cancel = [[Cancel]]
+LNG.exit_confirm = [[Haluatko varmasti lopettaa?]]
+LNG.confirm_resolution = [[Pidä uusi resoluutio?]]
+LNG.agree = [[Kyllä]]
+LNG.disagree = [[Ei]]
+LNG.cancel = [[Peruuta]]
 
 -------------------------------------------------
 -- ERRORS:
 -------------------------------------------------
-LNG.err_already_generating_map = [[Already generating new map! Wait for process to finish...]]
-LNG.err_wait_for_rendering = [[Wait for rendering to finish...]]
-LNG.err_rendering = [[Something went wrong in a thread while generating the map. Sorry about that, please restart the game and try again.]]
-LNG.err_already_connecting = [[Already attempting to start connection.]]
+LNG.err_already_generating_map = [[Luodaan uutta karttaa. Odota että prosessi valmistuu...]]
+LNG.err_wait_for_rendering = [[Odota että renderöinti valmistuu...]]
+LNG.err_rendering = [[Kartan luonti epäonnistui. Pahoittelut, käynnistä peli uudelleen ja yritä uudestaan.]]
+LNG.err_already_connecting = [[Yhteyden muodostaminen menossa]]
